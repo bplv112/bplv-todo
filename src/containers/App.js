070f3fad424457a90d6fc91dc69ee48ef41bb451
Biplav Subedi <biplav.subedi@incsub.com>
2020-11-18 13:24:08 +0545

    [Test] firebase-db

--- src/containers/App.js
+++ src/containers/App.js
@@ -9,23 +9,32 @@ import Tasks from '../components/Tasks/Tasks';
 import Todo from '../components/Todo/Todo';
 import Notice from '../components/Notice/Notice';
 import {getCurrentDate} from '../utils/Utils';
+import Loader from '../components/Loader/Loader';
+
+// Import the db handler.
+import DB from '../todo-axios';
 
 import './App.css';
 
 class App extends Component {
 
 	state = {
-	  tasks: [
-
-	   {time: '11/2020', task: 'Maxx',  done: true, id: 22},
-	   {time: '12/2020', task: 'bplv', done: false, id: 3},
-	   {time: '13/2020', task: 'hey',  done: false, id:7}
-
-	  ],
+	  tasks:null,
 	  tempTasks:{},
 	  alert:{variant:'',message:''},
 	  showAlert:false,
 	  setShowAlert:false,
+	  loading:true,
+	}
+
+	// Get data.
+	componentDidMount () {
+		DB.get('/todo-tasks.json')
+		.then(response => {
+			this.setState({tasks: response.data, loading:false});
+		})
+		.catch(error=>console.log(error));
+
 	}
 
 	// handler for deleting todo.
@@ -66,36 +75,61 @@ class App extends Component {
 			const state = {
 				...this.state
 			}
-			state.tasks.push( this.state.tempTasks );
+
+			// check empty values at the start.
+			if( ! state.tasks ) {
+				state.tasks = [{ ...this.state.tempTasks }];
+			} else {
+				state.tasks.push( this.state.tempTasks );
+			}
+
 			this.setState({tasks:state.tasks, tempTasks:'', alert:{variant:'success', message: 'Task Added'}, showAlert:true, setShowAlert: true});
+			this.updateDBHandler();
 		} else {
 			this.setState({alert:{variant:'danger', message: 'Nothing to add'}, showAlert:true, setShowAlert: true});
 		}
+	}
 
+	// Database update
+	updateDBHandler = () => {
+		let data = {
+			...this.state.tasks
+		}
+		console.log( data );
+		DB.post('/todo-bplv.json', data)
+		.then(response => console.log(response))
+		.catch(error=>console.log(error));
 	}
 
 	render() {
-	  return (
-		<Container className="p-3">
-		  <Jumbotron>
-			<h1 className="header d-flex justify-content-center">Your Todo-list</h1>
-			<Todo
-				tempTask={this.taskChangeHandler}
-				addTask={this.addTaskHandler}
-			></Todo>
+		if( this.state.loading ) {
+			return (
+				<Loader/>
+			);
+		} else {
+			console.log( this.state.tasks );
+			return (
+				<Container className="p-3">
+					<Jumbotron>
+						<h1 className="header d-flex justify-content-center">Your Todo-list</h1>
 
-			<Notice variant={this.state.alert.variant} message={this.state.alert.message}>
-		 	</Notice>
+						<Todo
+							tempTask={this.taskChangeHandler}
+							addTask={this.addTaskHandler}
+						/>
 
-			<Tasks
-				tasks={this.state.tasks}
-				deleted={this.deleteHandler}
-				done={this.doneHandler}
-			>
-			</Tasks>
-		  </Jumbotron>
-		</Container>
-	  );
+						<Notice variant={this.state.alert.variant} message={this.state.alert.message}/>
+
+						<Tasks
+							tasks={this.state.tasks ? this.state.tasks : []}
+							deleted={this.deleteHandler}
+							done={this.doneHandler}
+						/>
+
+					</Jumbotron>
+				</Container>
+			);
+		}
 	}
 }
 
